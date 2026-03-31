cc920b59d687966b468540285c3ab7b9d516c808
unknown <rajeshk09319@gmail.com>
2026-03-31 14:15:43 +0530

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,21 @@ def contact():
 @app.route("/service")
 def service():
     return render_template("service.html")
+
+@app.route("/menu", methods=["GET"])
+def get_menu():
+    return {
+        "items": ["Pizza", "Burger", "Pasta"]
+    }
+
+@app.route("/order", methods=["POST"])
+def create_order():
+    data = request.json
+    item = data["item"]
+
+    return {
+        "message": f"Order for {item} received!"
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
